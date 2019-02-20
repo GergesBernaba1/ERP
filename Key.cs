930cf4118da2813f11263bfbf891eafe9ab7fbed
GergesBernaba1 <gergesbernaba2@gmail.com>
2019-02-20 16:30:31 +0200

    Add files via upload

---
+++ Key.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mail;
+
+/// <summary>
+/// Summary description for Key
+/// </summary>
+public static class Key
+{
+    public static string _KeyValuePair(string key, string value)
+    {
+        return "{\"" + key + "\":\"" + value + "\"}";
+    }
+}
